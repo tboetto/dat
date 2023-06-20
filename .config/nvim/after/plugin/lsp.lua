9078d068fe278ee62e64954aaabca3729ac2d847
Tony Boetto <tony@tonyboetto.net>
2023-06-20 13:56:06 -0400

    updated nvim config

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,19 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+	'tsserver',
+	'eslint',
+	'angularls',
+	'lua_ls',
+})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
