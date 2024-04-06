b6d59283d702d983d680f6efa7f374a83a870c40
Tony Boetto <dev@tonyboetto.net>
2024-04-06 09:25:05 -0400

    update emacs init

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -119,32 +119,6 @@
 
 (use-package which-key
   :config (which-key-mode))
-(use-package undo-fu)
-;;; Vim Bindings
-(use-package evil
-  :demand t
-  :bind (("<escape>" . keyboard-escape-quit))
-  :init
-  ;; allows for using cgn
-  ;; (setq evil-search-module 'evil-search)
-  (setq evil-want-keybinding nil)
-  ;; no vim insert bindings
-  (setq evil-undo-system 'undo-fu)
-  :config
-  (evil-mode 1))
-
-;;; Vim Bindings Everywhere else
-(use-package evil-collection
-  :after evil
-  :config
-  (setq evil-want-integration t)
-  (evil-collection-init))
-
-(use-package evil-surround
-  :ensure t
-  :config
-  (global-evil-surround-mode 1))
-  
 (use-package doom-themes
   :config
   ;; Global settings (defaults)
@@ -170,13 +144,11 @@
   :ensure t
   :init (doom-modeline-mode 1))
 
-(use-package hydra)
-(use-package use-package-hydra)
-(use-package general)
 (use-package textsize
-   :custom (textsize-default-points (if (eq system-type 'darwin) 2 4))
+   :custom (textsize-default-points (if (eq system-type 'darwin) 24 28))
    :commands textsize-mode
    :init (textsize-mode))
+;;   :custom (textsize-default-points (if (eq system-type 'darwin) 2 4))
 ;;   :general (:prefix my-leader "t" 'my-hydra-textsize/body)
 ;;            ("C-x t" nil)
 ;;   :hydra (my-hydra-textsize (:exit nil :foreign-keys warn )
