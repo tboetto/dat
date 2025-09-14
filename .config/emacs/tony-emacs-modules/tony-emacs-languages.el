1d624c2ca5971b7e3e390c97826d5631bb2fdd00
Tony Boetto <tony@tonyboetto.net>
2025-09-14 14:17:47 -0400

    more language stuff

--- .config/emacs/tony-emacs-modules/tony-emacs-languages.el
+++ .config/emacs/tony-emacs-modules/tony-emacs-languages.el
@@ -3,4 +3,23 @@
   :ensure nil ;; built in
   :mode "\\.zsh\\'" "\\.sh\\'")
 
+(use-package aggressive-indent
+  :ensure t
+  :hook '(clojure-mode
+          elisp-mode
+          emacs-lisp-mode
+          lisp-mode
+          common-lisp-mode
+          scheme-mode))
+
+(use-package
+ smartparens
+ :ensure t
+ :hook
+ (clojure-mode . smartparens-mode)
+ (scheme-mode . smartparens-mode)
+ :config (require 'smartparens-config))
+
+
+
 (provide 'tony-emacs-languages)
